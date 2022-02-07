f7ce84ba6c053939f9e18ce750768f4369adad36
Seryn <schmitttni@gmail.com>
2022-02-07 01:00:40 +0100

    Completed sorting

--- app/src/main/java/com/example/studentoblig/DatabaseActivity.kt
+++ app/src/main/java/com/example/studentoblig/DatabaseActivity.kt
@@ -8,14 +8,20 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studentoblig.adapter.ItemAdapter
 import com.example.studentoblig.data.GlobalListHolder
+import com.example.studentoblig.model.Animal
 
 class DatabaseActivity : AppCompatActivity() {
+    private var sortAlphabetical: Boolean = true
+
+    private lateinit var myDataset: MutableList<Animal>
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_database)
 
-        val myDataset = GlobalListHolder.animalList
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        myDataset = GlobalListHolder.animalList
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
     }
 
@@ -28,10 +34,14 @@ class DatabaseActivity : AppCompatActivity() {
         Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
     }
 
+    //Sorts the names out of the StringResource, boolean which switches everytime
+    //to toggle between aphabetical and counteralphabetical
     fun sortData(view: View){
-        //GlobalListHolder.animalList.sortBy { it.stringResource.compareTo("a") }
-        val myDataset = GlobalListHolder.animalList
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
+        GlobalListHolder.animalList.sortWith(compareBy { it.stringResource })
+        if (!sortAlphabetical){
+            GlobalListHolder.animalList.reverse()
+        }
+        sortAlphabetical = !sortAlphabetical
+        recyclerView.adapter?.notifyDataSetChanged()
     }
 }
